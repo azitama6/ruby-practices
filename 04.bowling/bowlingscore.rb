33cb1724ffa181f03f630f9e4ce40f1774e83d06
yusuke.nakazato <ajitama6@gmail.com>
2021-02-21 01:53:53 +0900

    Xが2回続いた際に計算が狂う問題を修正

--- 04.bowling/bowlingscore.rb
+++ 04.bowling/bowlingscore.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+require 'pry'
+
+# ストライクの計算
+def strike_calculator(frames, nextframe)
+  # 次のフレームが１０
+  if frames[nextframe][0] == 10
+    10 + frames[nextframe][0] + frames[nextframe + 1][0]
+  # 次のフレームが１０以外
+  else
+    10 + frames[nextframe].sum
+  end
+end
+
 getscore = ARGV[0]
 scores = getscore.split(',')
 scores.map! { |x| x == 'X' ? '10' : x }
@@ -22,22 +35,21 @@ frames = []
 shots.each_slice(2) do |s|
   frames << s
 end
-
 calc = []
 # 現在フレーム数
-nowframe = 0
-
+current_frame = 0
 # 1~9フレームはスコア計算あり、１０フレーム以降計算なしで出力
 frames.each do |frame|
-  nowframe += 1
-  nextframe = nowframe + 1
-  if frame[0] == 10 && nowframe <= 9
-    10 + frames[nextframe].sum
-  elsif frame.sum == 10 && nowframe <= 9
-    10 + frames[nextframe][0]
-  else
-    frame.sum
-  end
-  calc << points
+  current_frame += 1
+  nextframe = current_frame
+  calc << if frame[0] == 10 && current_frame <= 9
+            strike_calculator(frames, nextframe)
+          elsif frame.sum == 10 && current_frame <= 9
+            10 + frames[nextframe][0]
+          else
+            frame.sum
+          end
 end
+
+# 計算結果を出力
 puts calc.sum
