5c6b641b055e63a8e08db37581e79b2829a97b97
yusuke.nakazato <ajitama6@gmail.com>
2021-03-04 13:34:06 +0900

    レビュー後修正内容を反映

--- 04.bowling/bowlingscore.rb
+++ 04.bowling/bowlingscore.rb
@@ -1,25 +1,17 @@
 # frozen_string_literal: true
 
-require 'pry'
-
-# ストライクの計算
-def strike_calculator(frames, nextframe)
-  # 次のフレームが１０
-  if frames[nextframe][0] == 10
-    10 + frames[nextframe][0] + frames[nextframe + 1][0]
-  # 次のフレームが１０以外
+def calculate_strike(frames, next_frame)
+  if frames[next_frame][0] == 10
+    10 + frames[next_frame][0] + frames[next_frame + 1][0]
   else
-    10 + frames[nextframe].sum
+    10 + frames[next_frame].sum
   end
 end
 
-getscore = ARGV[0]
-scores = getscore.split(',')
+scores = ARGV[0].split(',')
 
 shots = []
-# スコアを配列に入れる
 scores.each do |score|
-  # Xだったら10と０の要素を足す
   if score == 'X'
     shots << 10
     shots << 0
@@ -27,27 +19,17 @@ scores.each do |score|
     shots << score.to_i
   end
 end
-frames = []
 
-# 1フレームごとに配列を分ける
-shots.each_slice(2) do |s|
-  frames << s
-end
-calc = []
-# 現在フレーム数
-current_frame = 0
-# 1~9フレームはスコア計算あり、１０フレーム以降計算なしで出力
-frames.each do |frame|
-  current_frame += 1
-  nextframe = current_frame
-  calc << if frame[0] == 10 && current_frame <= 9
-            strike_calculator(frames, nextframe)
-          elsif frame.sum == 10 && current_frame <= 9
-            10 + frames[nextframe][0]
-          else
-            frame.sum
-          end
+frames = shots.each_slice(2).to_a
+
+results = frames.map.with_index(1) do |frame, next_frame|
+  if frame[0] == 10 && next_frame <= 9
+    calculate_strike(frames, next_frame)
+  elsif frame.sum == 10 && next_frame <= 9
+    10 + frames[next_frame][0]
+  else
+    frame.sum
+  end
 end
 
-# 計算結果を出力
-puts calc.sum
+puts results.sum
