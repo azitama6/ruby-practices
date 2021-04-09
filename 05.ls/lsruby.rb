8ea46893b98d1a96669e7d6245a9255ec3d9938e
yusuke.nakazato <ajitama6@gmail.com>
2021-04-09 13:35:57 +0900

    multipul_oputionの廃止,getoptsによるオプション取得のシンプル化

--- 05.ls/lsruby.rb
+++ 05.ls/lsruby.rb
@@ -4,22 +4,9 @@
 require 'optparse'
 require 'etc'
 
-def witch_permmisiion?(str)
-  file_types = {
-    '100' => '-',
-    '040' => 'd',
-    '120' => 'l'
-  }
-  permmisions = {
-    '0'	=> '---',
-    '1'	=> '--x',
-    '2'	=> '-w-',
-    '3'	=> '-wx',
-    '4'	=> 'r--',
-    '5'	=> 'r-x',
-    '6'	=> 'rw-',
-    '7'	=> 'rwx'
-  }
+def human_permmision(str)
+  file_types = {'100' => '-', '040' => 'd', '120' => 'l'}
+  permmisions = {'0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6'	=> 'rw-', '7'	=> 'rwx'}
   file_types[str[0, 3]] + permmisions[str[3, 1]] + permmisions[str[4, 1]] + permmisions[str[5, 1]]
 end
 
@@ -31,50 +18,22 @@ def puts_files(files)
   end
 end
 
-def puts_option_a(multiple_option_flag)
-  all_files = Dir.glob('*', File::FNM_DOTMATCH).sort
-  if multiple_option_flag
-    all_files
-  else
-    puts_files(all_files)
-  end
-end
-
-def puts_option_r(multiple_option_flag, result_files)
-  if multiple_option_flag
-    result_files.reverse
-  else
-    reversed_files = Dir.glob('*').sort.reverse
-    puts_files(reversed_files)
-  end
-end
-
-def puts_option_l(multiple_option_flag, result_files)
-  list_files = multiple_option_flag ? result_files : Dir.glob('*').sort
-  total = list_files.sum { |file| File::Stat.new(file).blocks }
+def puts_files_option_l(files)
+  total = files.sum { |file| File::Stat.new(file).blocks }
   puts "total #{total}"
-  list_files.each do |list_file|
-    stat = File.stat("./#{list_file}")
-    file_permission = witch_permmisiion?(stat.mode.to_s(8).rjust(6, '0'))
+  files.each do |file|
+    stat = File.stat("./#{file}")
+    file_permission = human_permmision(stat.mode.to_s(8).rjust(6, '0'))
     user = Etc.getpwuid(stat.uid).name
     group = Etc.getgrgid(stat.gid).name
     update_time = "#{stat.mtime.to_s[6, 1].rjust(2, ' ')} #{stat.mtime.to_s[9, 1].rjust(2, ' ')} #{stat.mtime.to_s[11, 5]}"
     size = stat.size.to_s.rjust(6, ' ')
-    puts "#{file_permission}  #{stat.nlink} #{user} #{group} #{size} #{update_time} #{list_file}"
+    puts "#{file_permission}  #{stat.nlink} #{user} #{group} #{size} #{update_time} #{file}"
   end
 end
-input_args = ARGV.map { |input_arg| input_arg.delete('-') }.join.chars.map { |arg| "-#{arg}" }
-option_args = []
-option_args << '-a' if input_args.include?('-a')
-option_args << '-r' if input_args.include?('-r')
-option_args << '-l' if input_args.include?('-l')
-multiple_option_flag = option_args.size > 1
 
-result_files = Dir.glob('*').sort
-opt = OptionParser.new
-opt.on('-a') { result_files = puts_option_a(multiple_option_flag) }
-opt.on('-r') { result_files = puts_option_r(multiple_option_flag, result_files) }
-opt.on('-l') { result_files = puts_option_l(multiple_option_flag, result_files) }
-opt.parse(option_args)
-puts_files(result_files) if option_args.empty?
-puts_files(result_files) if multiple_option_flag && !option_args.include?('-l')
+options = ARGV.getopts('a', 'r', 'l')
+files = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
+files = files.reverse if options['r']
+
+options['l'] ? puts_files_option_l(files) : puts_files(files)
