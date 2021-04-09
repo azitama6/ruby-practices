e211eaf64ebcc745dcf7c06a887dc1c17d5d5940
yusuke.nakazato <ajitama6@gmail.com>
2021-04-09 15:49:41 +0900

    permissionの表記誤字を修正し統一

--- 05.ls/lsruby.rb
+++ 05.ls/lsruby.rb
@@ -4,10 +4,10 @@
 require 'optparse'
 require 'etc'
 
-def human_permmision(str)
-  file_types = {'100' => '-', '040' => 'd', '120' => 'l'}
-  permmisions = {'0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6'	=> 'rw-', '7'	=> 'rwx'}
-  file_types[str[0, 3]] + permmisions[str[3, 1]] + permmisions[str[4, 1]] + permmisions[str[5, 1]]
+def human_permission(str)
+  file_types = { '100' => '-', '040' => 'd', '120' => 'l' }
+  permissions = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
+  file_types[str[0, 3]] + permissions[str[3, 1]] + permissions[str[4, 1]] + permissions[str[5, 1]]
 end
 
 def puts_files(files)
@@ -23,7 +23,7 @@ def puts_files_option_l(files)
   puts "total #{total}"
   files.each do |file|
     stat = File.stat("./#{file}")
-    file_permission = human_permmision(stat.mode.to_s(8).rjust(6, '0'))
+    file_permission = human_permission(stat.mode.to_s(8).rjust(6, '0'))
     user = Etc.getpwuid(stat.uid).name
     group = Etc.getgrgid(stat.gid).name
     update_time = "#{stat.mtime.to_s[6, 1].rjust(2, ' ')} #{stat.mtime.to_s[9, 1].rjust(2, ' ')} #{stat.mtime.to_s[11, 5]}"
@@ -35,5 +35,4 @@ end
 options = ARGV.getopts('a', 'r', 'l')
 files = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
 files = files.reverse if options['r']
-
 options['l'] ? puts_files_option_l(files) : puts_files(files)
